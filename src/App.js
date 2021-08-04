caa76580af8031b9da0342baeddd45a8b3400311
oscar Vaquera <vaqueraoscar0@gmail.com>
2021-08-03 19:06:42 -0700

    Added last needed components, integrated Clarifai API [FaceRecognition]

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import ImageLinkForm from './Component/ImageLinkForm/ImageLinkForm';
 import Navigation from "./Component/Navigation/Navigation";
 import Logo from "./Component/Logo/Logo";
 import Rank from "./Component/Rank/Rank";
+import Clarifai from 'clarifai';
 import './App.css';
 import Particles from "react-particles-js";
+import FaceRecognition from "./Component/FaceRecognition/FaceRecognition";
+
+const app = new Clarifai.App({
+    apiKey: '0d63782af1044abf968675c7809a0285'
+});
 
 const particleOptions = {
     particles: {
@@ -12,13 +18,37 @@ const particleOptions = {
             value: 30,
             density:{
                 enable: true,
-                value_area: 800
+                value_area: 200
             }
         }
     }
 }
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageURL: ''
+        }
+    }
+
+   onInputChange = (event) =>{
+        this.setState({input: event.target.value});
+    }
+
+    onButtonSubmit =() =>{
+        this.setState({imageURL: this.state.input})
+        app.models.predict( Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function (response){
+                console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+            },
+            function(err){
+
+            }
+        );
+    }
+
   render() {
     return (
         <div className="App">
@@ -28,8 +58,10 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm />
-          {/*<FaceRecognition />*/}
+          <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}/>
+          <FaceRecognition imageURL={this.state.imageURL}/>
 
         </div>
     );
